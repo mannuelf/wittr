961121d6e928ee027e6867f4afa7bceefbe94eaa
manidf <mannuel.ferreira@gmail.com>
2016-09-10 14:44:08 +0200

    repsond to url with specific parameters

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,8 +1,22 @@
 self.addEventListener('fetch', function(event) {
-   // tell browser we will handle this event ouselves.
+   // Tell browser we will handle this event ouselves.
    // event.respondWith takes a repsonse object or a promise that resolve with a repsonse
-   event.respondWith(
+   /*event.respondWith(
        // can take blob, buffer string or other data
-       new Response('Hello World!')
-   );
+       new Response('Hello <b class="a-winner-is-me">World!</b>', {
+           // change the headers and content type
+           headers: { 'Content-Type': 'text/html; charset=utf-8' }
+       })       
+   );*/
+ 
+   // respond to request if ends in .jpg
+   if ( event.request.url.endsWith('.jpg') ) {
+        // Fetch an image with fetch API
+        event.respondWith(
+            fetch('/imgs/dr-evil.gif')
+        );
+    }
+
+   console.log(event.request);
+
 });
