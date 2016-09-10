5c891c1d14c5f18622cc7b2688cb1de2cc6343cf
manidf <mannuel.ferreira@gmail.com>
2016-09-10 13:26:21 +0200

    capture a request

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,3 +1,8 @@
 self.addEventListener('fetch', function(event) {
-   console.log(event.request);  
+   // tell browser we will handle this event ouselves.
+   // event.respondWith takes a repsonse object or a promise that resolve with a repsonse
+   event.respondWith(
+       // can take blob, buffer string or other data
+       new Response('Hello World!')
+   );
 });
